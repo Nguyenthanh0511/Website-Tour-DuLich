cc8b6e17c84e626d7e43f31dcbd15550482a424a
Nguyenthanh04 <tytytititity@gmail.com>
2023-12-10 18:50:24 +0700

    Initial project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Hero />
+  </div>
+</template>
+
+
+<script>
+import Hero from "./components/HeroComponent.vue";
+
+export default {
+  components: { Hero },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
